3bc0ce40009057bb2c3c43d454434e1cfb34eec6
BenRodProd <125862830+BenRodProd@users.noreply.github.com>
2023-09-26 22:32:09 +0200

    cross-opener-policy

--- next.config.js
+++ next.config.js
@@ -14,21 +14,5 @@ const nextConfig = {
       }
 }
 
-module.exports = {
-  nextConfig,
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin", // "same-origin-allow-popups"
-          },
-        ],
-      },
-    ];
-  },
-}
-
-
+module.exports = nextConfig
+  
