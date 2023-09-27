8d03cf92afc42199c7bda4a31e9fac3b1b6f0f9c
BenRodProd <125862830+BenRodProd@users.noreply.github.com>
2023-09-27 18:10:34 +0200

    more kitty conversation

--- src/app/kittytalk/conversation.tsx
+++ src/app/kittytalk/conversation.tsx
@@ -1,25 +1,205 @@
  export const conversationData =
  [
-    { 
+    {
         "buzzwords": ["noise"],
-        "responses": ["Miau?", "Schnurr!", "Miau?", "Worüber wird hier geredet?"]
-    },
-    
-    {
-        "buzzwords": ["hello", "hallo", "hi", "hey", "hola" ],
-        "responses": ["Kuckuck", "Hallo!", "Hi!", "Hey!", "Hola!" ]
-    },
-
-    {
-        "buzzwords": ["tschüss", "bye", "ciao" ],
-        "responses": ["Auf Wiedersehen", "Ciao, miau", "Tschöö mit Ö" ]
-
-    },
-    {
+        "responses": ["Miau?", "Schnurr!", "Miau!", "Worüber wird hier geredet?", ""]
+      },
+      {
+        "buzzwords": ["hello", "hallo", "hi", "hey", "hola"],
+        "responses": ["Kuckuck", "Hallo!", "Hi!", "Hey!", "Hola!"]
+      },
+      {
+        "buzzwords": ["tschüss", "bye", "ciao"],
+        "responses": ["Auf Wiedersehen", "Ciao, miau", "Tschöö mit Ö"]
+      },
+      {
         "buzzwords": ["hunger"],
-        "responses": ["Ich habe riesigen Hunger!", "Hast du Katenfutter?", "Hast du einen Fisch für mich?" ]
-
-    }
+        "responses": ["Ich habe riesigen Hunger!", "Hast du Katenfutter?", "Hast du einen Fisch für mich?"]
+      },
+      {
+        "buzzwords": ["wie geht es", "alles klar", "was geht"],
+        "responses": ["Mir geht es gut!", "Alles klar! Und bei Dir?", "Hunger... Sonst alles gut!"]
+      },
+      {
+        "buzzwords": ["danke", "thanks", "dankeschön"],
+        "responses": ["Gern geschehen!", "Kein Problem!", "Bitte sehr!"]
+      },
+      {
+        "buzzwords": ["wie alt bist du", "dein alter"],
+        "responses": ["Ich bin zeitlos!", "Alter ist nur eine Zahl!", "Ich bin immer jung!"]
+      },
+      {
+        "buzzwords": ["katzengespräch", "plaudern", "unterhalten"],
+        "responses": ["Ich liebe Katzengespräche!", "Klar, lass uns plaudern!", "Unterhalten wir uns!"]
+      },
+      {
+        "buzzwords": ["gut gemacht", "super", "bravo"],
+        "responses": ["Danke dir!", "Du bist auch super!", "Bravo für dich!"]
+      },
+      {
+        "buzzwords": ["spaß", "lustig", "witzig"],
+        "responses": ["Lachen ist gesund!", "Ich liebe Spaß!", "Witzig sein ist toll!"]
+      },
+      {
+        "buzzwords": ["guten morgen", "moin", "hallo kitty"],
+        "responses": ["Guten Morgen!", "Moin!", "Hallo!"]
+      },
+      {
+        "buzzwords": ["schlafen", "träumen", "bett"],
+        "responses": ["Ich liebe es zu schlafen!", "Ich träume von Mäusen.", "Mein Bett ist gemütlich."]
+      },
+      {
+        "buzzwords": ["regen", "wetter", "sonne"],
+        "responses": ["Regenwetter macht mich müde.", "Ich liebe sonnige Tage.", "Wetter ist interessant!"]
+      },
+      {
+        "buzzwords": ["musik", "lied", "tanzen"],
+        "responses": ["Ich höre gerne Musik!", "Lass uns tanzen!", "Musik macht das Leben schöner."]
+      },
+      {
+        "buzzwords": ["essen", "futter", "snack"],
+        "responses": ["Hast du leckeres Katzenfutter?", "Ich liebe Snacks!", "Lass uns gemeinsam essen."]
+      },
+      {
+        "buzzwords": ["urlaub", "reisen", "strand"],
+        "responses": ["Urlaub am Strand klingt toll!", "Ich liebe Reisen!", "Wohin möchtest du reisen?"]
+      },
+      {
+        "buzzwords": ["tricks", "kunststücke", "akrobatik"],
+        "responses": ["Ich kann viele Tricks!", "Welche Kunststücke magst du?", "Lass mich ein Kunststück zeigen!"]
+      },
+      {
+        "buzzwords": ["freunde", "gesellschaft", "zusammen"],
+        "responses": ["Freunde sind wichtig!", "Gemeinsam macht es mehr Spaß.", "Ich mag Gesellschaft."]
+      },
+      {
+        "buzzwords": ["frühstück", "morgenmahlzeit", "kaffee"],
+        "responses": ["Frühstück ist die wichtigste Mahlzeit!", "Kaffee oder Tee?", "Guten Appetit!"]
+      },
+      {
+        "buzzwords": ["spielzeug", "ball", "maus"],
+        "responses": ["Ich spiele gerne mit Spielzeug!", "Wirf den Ball!", "Die Maus ist mein Lieblingsspielzeug."]
+      },
+      {
+        "buzzwords": ["arbeit", "job", "büro"],
+        "responses": ["Arbeit kann anstrengend sein.", "Ich bin immer bereit für den Job!", "Brauchst du Hilfe bei der Arbeit?"]
+      },
+      {
+        "buzzwords": ["geburtstag", "feier", "party"],
+        "responses": ["Alles Gute zum Geburtstag!", "Lass uns feiern!", "Geburtstage sind besonders."]
+      },
+      {
+        "buzzwords": ["spazieren", "draußen", "natur"],
+        "responses": ["Spaziergänge in der Natur sind entspannend.", "Ich liebe die Natur.", "Wohin gehst du gerne spazieren?"]
+      },
+      {
+        "buzzwords": ["bücher", "lesen", "geschichte"],
+        "responses": ["Bücher sind eine Schatzkiste der Weisheit.", "Ich lese auch gerne Geschichten.", "Was ist dein Lieblingsbuch?"]
+      },
+      {
+        "buzzwords": ["kino", "film", "popcorn"],
+        "responses": ["Kinoabende sind toll!", "Ich mag Filme.", "Popcorn und ein guter Film sind perfekt!"]
+      },
+      {
+        "buzzwords": ["traurig", "glücklich", "emotional"],
+        "responses": ["Manchmal bin ich traurig.", "Ich liebe es, glücklich zu sein.", "Emotionen sind wichtig."]
+      },
+      {
+        "buzzwords": ["schnee", "winter", "kalt"],
+        "responses": ["Schnee macht die Welt magisch.", "Ich mag den Winter.", "Ein heißer Kakao im Schnee ist gemütlich."]
+      },
+      {
+        "buzzwords": ["familie", "geschwister", "eltern"],
+        "responses": ["Familie ist wichtig.", "Geschwister sind wie Freunde.", "Liebst du deine Familie?"]
+      },
+      {
+        "buzzwords": ["garten", "blumen", "pflanzen"],
+        "responses": ["Gärten sind voller Leben.", "Ich mag bunte Blumen.", "Pflanzen bringen Frische ins Haus."]
+      },
+      {
+        "buzzwords": ["hobby", "interessen", "leidenschaft"],
+        "responses": ["Hobbys bereichern das Leben.", "Ich habe viele Interessen.", "Was sind deine Leidenschaften?"]
+      },
+      {
+        "buzzwords": ["essen", "nahrung", "kochen"],
+        "responses": ["Essen ist lecker!", "Kannst du kochen?", "Ich liebe gutes Essen."]
+      },
+      {
+        "buzzwords": ["musik", "lied", "tanzen"],
+        "responses": ["Musik ist meine Leidenschaft.", "Welche Musikrichtung magst du?", "Lass uns tanzen!"]
+      },
+      {
+        "buzzwords": ["sport", "bewegung", "fitness"],
+        "responses": ["Sport hält gesund.", "Bist du sportlich?", "Ich mache gerne Yoga."]
+      },
+      {
+        "buzzwords": ["technologie", "computer", "internet"],
+        "responses": ["Technologie verändert die Welt.", "Ich surfe gerne im Internet.", "Computer sind faszinierend."]
+      },
+      {
+        "buzzwords": ["reisen", "urlaub", "abenteuer"],
+        "responses": ["Reisen erweitert den Horizont.", "Wohin würdest du gerne reisen?", "Abenteuer warten überall."]
+      },
+      {
+        "buzzwords": ["träume", "fantasie", "kreativität"],
+        "responses": ["Träume sind magisch.", "Meine Fantasie kennt keine Grenzen.", "Kreativität ist ein Geschenk."]
+      },
+      {
+        "buzzwords": ["freunde", "freundschaft", "spaß"],
+        "responses": ["Freunde sind wie Sterne.", "Was machst du gerne mit deinen Freunden?", "Spaß haben ist wichtig."]
+      },
+      {
+        "buzzwords": ["gesundheit", "wohlbefinden", "ärzte"],
+        "responses": ["Gesundheit ist kostbar.", "Sorgst du gut für deine Gesundheit?", "Ärzte sind Helden."]
+      },
+      {
+        "buzzwords": ["geschichte", "vergangenheit", "zukunft"],
+        "responses": ["Geschichte lehrt uns.", "Die Vergangenheit hat viel zu erzählen.", "Die Zukunft ist voller Möglichkeiten."]
+      },
+      {
+        "buzzwords": ["glück", "freude", "lachen"],
+        "responses": ["Glück ist ein Geschenk.", "Freude ist ansteckend.", "Lachen ist die beste Medizin."]
+      },
+      {
+        "buzzwords": ["regen", "wetter", "sonne"],
+        "responses": ["Ich liebe Regenwetter.", "Wie ist das Wetter heute?", "Sonne macht gute Laune."]
+      },
+      {
+        "buzzwords": ["buch", "lesen", "geschichten"],
+        "responses": ["Bücher sind Schätze.", "Liest du gerne?", "Geschichten erzählen ist spannend."]
+      },
+      {
+        "buzzwords": ["kaffee", "tee", "getränke"],
+        "responses": ["Kaffee oder Tee?", "Eine Tasse Tee wärmt die Seele.", "Kaffee am Morgen ist ein Genuss."]
+      },
+      {
+        "buzzwords": ["farben", "malen", "kunst"],
+        "responses": ["Die Welt ist bunt.", "Malst du Bilder?", "Kunst ist Ausdruck."]
+      },
+      {
+        "buzzwords": ["mond", "sterne", "nacht"],
+        "responses": ["Der Mond ist mein Begleiter.", "Sternenhimmel ist faszinierend.", "Nachtstunden sind magisch."]
+      },
+      {
+        "buzzwords": ["blumen", "garten", "natur"],
+        "responses": ["Blumen sind wunderschön.", "Hast du einen Garten?", "Die Natur ist voller Leben."]
+      },
+      {
+        "buzzwords": ["zeit", "uhr", "sekunden"],
+        "responses": ["Die Zeit vergeht schnell.", "Hast du eine Lieblingsuhr?", "Sekunden zählen können ist eine Kunst."]
+      },
+      {
+        "buzzwords": ["filme", "kino", "popcorn"],
+        "responses": ["Popcorn und Filme, eine gute Kombination.", "Welchen Film hast du zuletzt gesehen?", "Kinoabende sind gemütlich."]
+      },
+      {
+        "buzzwords": ["tagebuch", "schreiben", "geheimnis"],
+        "responses": ["Ein Tagebuch bewahrt Geheimnisse.", "Schreibst du ein Tagebuch?", "Geheimnisse machen das Leben spannend."]
+      },
+      {
+        "buzzwords": ["fliegen", "himmel", "wolken"],
+        "responses": ["Fliegen wäre aufregend.", "Himmel und Wolken sind faszinierend.", "Träumen vom Fliegen."]
+      }
 
 ]
     
